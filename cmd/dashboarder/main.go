3adab773648412f416aaecf7a553e650e83a17d9
Vilem Kebrt <viker@viker.cz>
2025-01-12 11:46:39 +0100

    Update main.go

--- cmd/dashboarder/main.go
+++ cmd/dashboarder/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-		lf, err := os.OpenFile("application.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0640)
+	lf, err := os.OpenFile("application.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0640)
 	if err != nil {
 		panic(err)
 	}
