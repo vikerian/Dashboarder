41519c3be8885374925ec0a1721337e6c44f2182
viker <kebrtv@gmail.com>
2025-03-22 22:28:20 +0100

    2025-03-22 22:30

--- mqtt/mqtt.go
+++ mqtt/mqtt.go
@@ -2,10 +2,10 @@ package mqtt
 
 import (
 	"crypto/tls"
-	"crypto/x509"
+//	"crypto/x509"
 	"fmt"
-	"io/ioutil"
-	"log"
+//	"io/ioutil"
+//	"log"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	uuid "github.com/google/uuid"
@@ -17,33 +17,37 @@ const CaCrtPath string = "./ca.pem"
 var mqttUrl string
 
 type Mqtt struct {
-	Url       string // Url is tcp://user:password@host:port
+	URL       string // Url is tcp://user:password@host:port
 	ClientID  string // Set on Newclient, otherwise generated uuid -> last 8 chars without --
-	opts      *mqtt.ClientOptions
+	Opts      *mqtt.ClientOptions
 	Topics    []string //
 	caCrtPATH string
 	CaCRT     []byte
-	TlsConfig *tls.Config
-	Client    mqtt.Client
+	TlsCONFIG *tls.Config
+	CLH    mqtt.Client
 }
 
-/* NewClient => constructor
+/* New => constructor
  *	-> InputParams:
  *		string uri -> url in format tcp://user:pass@host:port
  *		string caCrtPath -> path on filesystem to CA certificate (yes have to be mounted RO to container)
  *	-> return clean instance filled with params from constructor
  */
-func NewClient(uri, caCrtPath string) (*Mqtt, error) {
+func New(uri, caCrtPath string) (mq Mqtt,err error) {
 	id, err := uuid.NewUUID()
 	if err != nil {
-		return nil, err
+		return 
 	}
 	clientID := fmt.Sprintf("%s-%s", ClientPrefix, id.String()[4:8])
-	return &Mqtt{
-		Url:       uri,
-		ClientID:  clientID,
-		caCrtPATH: caCrtPath,
-	}, nil
+	mq.URL = uri
+	mq.ClientID = clientID
+	mq.caCrtPATH = caCrtPath
+	mq.Opts = mqtt.NewClientOptions().AddBroker(mq.URL).SetClientID(clientID)
+	mq.CLH = mqtt.NewClient(mq.Opts)
+	if token := mq.CLH.Connect(); token.Wait() && token.Error() != nil {
+		return
+	}
+	return
 }
 
 /* Connect -> connect to mqtt and get version info
@@ -63,3 +67,5 @@ func newTlsConfig() *tls.Config {
 		InsecureSkipVerify: true,
 	}
 }
+
+*/
