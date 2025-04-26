bc8861d9faa6921c78e73960193d833a8f8e1e3f
vilem_kebrt <vilem.kebrt@dcuk.cz>
2025-04-26 13:10:27 +0200

    2025-04-26 09:00

--- src/config.go
+++ src/config.go
@@ -1,13 +1,15 @@
 package main
 
+import "os"
+
 type Config struct {
-	MqttURL         string
-	MqttAUTH        string
-	MqttChannelLIST []string
-	ValkeyURL       string
-	ValkeyAUTH      string
-	SiriDbURL       string
-	SiriDbAUTH      string
+	MqttURL         string   `json:"mqtt_url"`
+	MqttAUTH        string   `json:"mqtt_auth"`
+	MqttChannelLIST []string `json:"mqtt_channels"`
+	ValkeyURL       string   `json:"valkey_url"`
+	ValkeyAUTH      string   `json:"valkey_auth"`
+	SiriDbURL       string   `json:"siridb_url"`
+	SiriDbAUTH      string   `json:"siridb_auth"`
 }
 
 func NewConfig() Config {
@@ -16,6 +18,7 @@ func NewConfig() Config {
 }
 
 func (cf *Config) LoadConfiguration() (ok bool, err error) {
-
+	cf.MqttURL,ok = os.Lookupenv("MQTT_URL")
+	cf.MqttAUTH,ok = os.
 	return
 }
