333f1080d4971bf452a3b399094d002988d0e13c
viker <kebrtv@gmail.com>
2025-05-06 21:16:51 +0200

    2025-05-06 21:16

--- src/config.go
+++ src/config.go
@@ -10,67 +10,83 @@ import (
 )
 
 // types
+
+// Config -> configuration options loaded from environment by method
 type Config struct {
-	MqttUrl         string      `json:"mqtt_url"`
-	MqttAuth        interface{} `json:"mqtt_auth"`
+	MqttURL         string      `json:"mqtt_url"`
+	MqttAUTH        interface{} `json:"mqtt_auth"`
 	MqttChannelLIST []string    `json:"mqtt_channels"`
-	ValkeyUrl       string      `json:"valkey_url"`
-	ValkeyAuth      interface{} `json:"valkey_auth"`
-	SiridbUrl       string      `json:"siridb_url"`
-	SiridbAuth      interface{} `json:"siridb_auth"`
+	ValkeyURL       string      `json:"valkey_url"`
+	ValkeyAUTH      interface{} `json:"valkey_auth"`
+	SiriURL         string      `json:"siridb_url"`
+	SiriAUTH        interface{} `json:"siridb_auth"`
+	MongoURL        string      `json:"mongo_url"`
+	MongoAUTH       interface{} `json:"mongo_auth"`
 }
 
-type AuthVar struct {
+// AuthVAR -> authentication data decoded
+type AuthVAR struct {
 	User string
 	Pass string
 }
 
-// constructors
+// NewConfig -> constructor of configuration data instance
 func NewConfig() Config {
 	cf := new(Config)
 	return *cf
 }
 
-// methods
+// LoadConfiguration -> load configuration into our instance from environment (do checks if values exists)
 func (cf *Config) LoadConfiguration() (err error) {
 	var ok bool
-	cf.MqttUrl, ok = os.LookupEnv("MQTT_URL")
+	cf.MqttURL, ok = os.LookupEnv("MQTT_URL")
 	if !ok {
 		return errors.New("Missing MQTT_URL in environment")
 	}
 
-	cf.MqttAuth, ok = os.LookupEnv("MQTT_AUTH")
+	cf.MqttAUTH, ok = os.LookupEnv("MQTT_AUTH")
 	if !ok {
 		return errors.New("Missing or nondecodable MQTT_AUTH in environment")
 	}
 
-	cf.ValkeyUrl, ok = os.LookupEnv("VALKEY_URL")
+	cf.ValkeyURL, ok = os.LookupEnv("VALKEY_URL")
 	if !ok {
 		return errors.New("Missing VALKEY_URL in environment")
 	}
 
-	cf.ValkeyAuth, ok = os.LookupEnv("VALKEY_AUTH")
+	cf.ValkeyAUTH, ok = os.LookupEnv("VALKEY_AUTH")
 	if !ok {
 		return errors.New("Missing VALKEY_AUTH in environment")
 	}
 
-	cf.SiridbUrl, ok = os.LookupEnv("SIRIDB_URL")
+	cf.SiriURL, ok = os.LookupEnv("SIRIDB_URL")
 	if !ok {
 		return errors.New("Missing SIRIDB_URL in environment")
 	}
 
-	cf.SiridbAuth, ok = os.LookupEnv("SIRIDB_AUTH")
+	cf.SiriAUTH, ok = os.LookupEnv("SIRIDB_AUTH")
 	if !ok {
-		return errors.New("Missing SIRIDB_AUTHh in environment")
+		return errors.New("Missing SIRIDB_AUTH in environment")
+	}
+
+	cf.MongoURL, ok = os.LookupEnv("MONGODB_URL")
+	if !ok {
+		return errors.New("Missing MONGODB_URL in environment")
+	}
+
+	cf.MongoAUTH, ok = os.LookupEnv("MONGODB_AUTH")
+	if !ok {
+		return errors.New("Missing MONGODB_AUTHh in environment")
 	}
 	return
 }
 
-func DecodeAuth(in string) (av AuthVar, err error) {
+// DecodeAuth -> deocde authentication from b64, return decoded data as AuthVAR instance
+func DecodeAuth(in string) (av AuthVAR, err error) {
 	var buffer bytes.Buffer
 	data, err := base64.StdEncoding.DecodeString(in)
 	if err != nil {
-		err = errors.New(fmt.Sprintf("Error on decoding auth: %v", err))
+		err = fmt.Errorf("Error on decoding auth: %v", err)
 		return
 	}
 	buffer.Write(data)
