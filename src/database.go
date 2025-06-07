4206660aabcd74b7435dc9a68a3a6dfbfb445b8f
vilem_kebrt <vilem.kebrt@dcuk.cz>
2025-06-07 17:43:58 +0200

    2025-0607 15:44

--- src/database.go
+++ src/database.go
@@ -1,6 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	//	"go.mongodb.org/mongo-driver/bson"
+	//	"go.mongodb.org/mongo-driver/bson/primitive"
+	//	"go.mongodb.org/mongo-driver/mongo"
+	//	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
 // DatabaseType represents different types of db
 type DatabaseType int
@@ -29,10 +41,17 @@ type MongoDBConnection struct {
 	URI        string
 	Database   string
 	Collection string
+	Ctx        *context.Context
+	Cnc        *context.CancelFunc
+	Clh        *mongo.Client
+	ClientOpt  *options.Options
 }
 
 func (m *MongoDBConnection) Connect() error {
 	fmt.Println("Connecting  to MongoDB:", m.URI)
+	ctx, canc := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
+	m.Ctx = &ctx
+	m.Cnc = &canc
 
 	return nil
 }
