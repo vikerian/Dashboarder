65182c125428a37f62f650ce87a31fcde75c91b0
Viker <vilem.kebrt@portabo.cz>
2025-06-10 15:22:04 +0200

    2025-06-10 15:22

--- src/database.go
+++ src/database.go
@@ -1,19 +1,9 @@
 package main
 
-import (
-	"context"
-	"errors"
-	"fmt"
-	"time"
-
-	//	"go.mongodb.org/mongo-driver/bson"
-	//	"go.mongodb.org/mongo-driver/bson/primitive"
-	//	"go.mongodb.org/mongo-driver/mongo"
-	//	"go.mongodb.org/mongo-driver/mongo/options"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
+//	"go.mongodb.org/mongo-driver/bson"
+//	"go.mongodb.org/mongo-driver/bson/primitive"
+//	"go.mongodb.org/mongo-driver/mongo"
+//	"go.mongodb.org/mongo-driver/mongo/options"
 
 // DatabaseType represents different types of db
 type DatabaseType int
@@ -32,56 +22,12 @@ type DatabaseConnection interface {
 	Get(string) (DataItem, error)
 }
 
-// MongoDBConnection implements DatabaseConnection for MongoDB
-type MongoDBConnection struct {
-	URI        string
-	Database   string
-	Collection string
-	Ctx        context.Context
-	Cnc        context.CancelFunc
-	Clh        *mongo.Client
-	Cursor     *mongo.Collection
-}
-
-// Connect - create connection to mongodb and verify it
-func (m *MongoDBConnection) Connect(uri string, dbname string, collection string) (err error) {
-	m.URI = uri
-	m.Database = dbname
-	m.Collection = collection
-	fmt.Println("Connecting  to MongoDB:", m.URI)
-	ctx, canc := context.WithTimeout(context.Background(), time.Duration(10)*time.Second)
-	m.Ctx = ctx
-	m.Cnc = canc
-	// create client with url - connection setup
-	m.Clh, err = mongo.Connect(m.Ctx, options.Client().ApplyURI(m.URI))
-	if err != nil {
-		err = errors.New(fmt.Sprintf("Error on connection to MongoDB: %v", err))
-		return
-	}
-	// verify connection as is
-	err = m.Clh.Ping(m.Ctx, nil)
-	if err != nil {
-		err = errors.New(fmt.Sprintf("Error on verifying connection to MongoDB: %v", err))
-	}
-	//create cursor
-	m.Cursor = m.Clh.Database(m.Database).Collection(m.Collection)
-	return
-}
-
-// Disconnect -> disconnect actual mongo connection
-func (m *MongoDBConnection) Disconnect() (err error) {
-	fmt.Println("Disconnecting from MongoDB")
-	err = m.Clh.Disconnect(m.Ctx)
-	return
-}
-
-// Store data into mongodb, returns key (_id) of documen /  error
-func (m *MongoDBConnection) Store(data DataItem) (key interface{}, err error) {
-
-	return
-}
-
-func (m *MongoDBConnection) Get(key interface{}) (data DataItem, err error) {
-
-	return
+type DatabaseConnectionInfo struct {
+	DbTYPE   int
+	DSN      string // dsn = database connection string in url format: mongodb://user@pass@fqdn:port/database
+	Username string
+	Password string
+	Token    string
+	Database string
+	Table    string
 }
